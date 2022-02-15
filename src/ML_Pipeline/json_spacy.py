9f922f0238c508f9efc7be386b1e442feecd5a87
bouchra07 <bouchra0805@gmail.com>
2022-02-15 12:31:11 +0100

    convert training data to spacy json format

---
+++ src/ML_Pipeline/json_spacy.py
@@ -0,0 +1,31 @@
+import json
+import logging
+
+def convert_data_to_spacy(JSON_FilePath):
+    try:
+        training_data = []
+        lines = []
+        with open(JSON_FilePath, 'r', encoding='utf-8') as f:
+            lines = f.readlines()
+
+        for line in lines:
+            data = json.loads(line)
+            text = data['content']
+            entities = []
+            for annotation in data['annotation']:
+                # only a single point in text annotation.
+                point = annotation['points'][0]
+                labels = annotation['label']
+                # handle both list of labels or a single label.
+                if not isinstance(labels, list):
+                    labels = [labels]
+
+                for label in labels:
+                    entities.append((point['start'], point['end'] + 1, label))
+
+            training_data.append((text, {"entities": entities}))
+
+        return training_data
+    except Exception as e:
+        logging.exception("Unable to process " + JSON_FilePath + "\n" + "error = " + str(e))
+        return None
